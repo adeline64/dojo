d325ad6e19dc2cda2d1a1fc71372390ed323a0ef
adeline64 <adeline.lacabanne@gmail.com>
2020-01-07 12:23:23 +0100

    js

--- dojo2/js/dojosjs.js
+++ dojo2/js/dojosjs.js
@@ -7,16 +7,47 @@ document.querySelector("main > section > aside > div").onmouseleave = function()
 {     this.style.borderRadius = "50%"; };
 
 //exercice 2
-let d1= document.querySelector("main section section article div");
-let tet = document.querySelector("main section section article header").addEventListener("click", () => {
-    if (d1.style.visibility  === "hidden") {
-        d1.style.visibility = "visible";
-        d1.style.height = "auto";
+
+let images= document.querySelectorAll("main section section article div")
+let fleche = document.querySelectorAll("main section section article header")
+fleche[0].addEventListener("click", () => {
+    if (images[0].style.visibility  === "hidden") {
+        images[0].style.visibility = "visible";
+        images[0].style.height = "auto";
       } else {
-        d1.style.visibility  = "hidden";
-        d1.style.height = "0px";
+        images[0].style.visibility  = "hidden";
+        images[0].style.height = "0px";
       }
     }
-  );
+  )
+
+fleche[1].addEventListener("click", () => {
+    if (images[1].style.visibility  === "hidden") {
+        images[1].style.visibility = "visible";
+        images[1].style.height = "auto";
+      } else {
+        images[1].style.visibility  = "hidden";
+        images[1].style.height = "0px";
+      }
+    }
+  )
+
+  fleche[2].addEventListener("click", () => {
+    if (images[2].style.visibility  === "hidden") {
+        images[2].style.visibility = "visible";
+        images[2].style.height = "auto";
+      } else {
+        images[2].style.visibility  = "hidden";
+        images[2].style.height = "0px";
+      }
+    }
+  )
+
+  
+
+
 
 //exercice 3
+
+let menu;
+
